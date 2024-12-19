73346b1875248861893edcfcd603dc1cfb5ee015
TheBigMouse-Fstack <khanhnqse19@gmail.com>
2024-12-19 10:52:07 +0700

    initialize environment

---
+++ FE/tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontSize: {
+        base: "1rem",
+      },
+    },
+  },
+  plugins: [],
+};
